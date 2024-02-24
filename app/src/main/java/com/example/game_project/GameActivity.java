c0ffd3159373e668a9dee8fcb5235087121d6869
CkVaZZaR <supervovan770@gmail.com>
2024-02-24 10:43:35 +0300

    1.1

--- app/src/main/java/com/example/game_project/GameActivity.java
+++ app/src/main/java/com/example/game_project/GameActivity.java
@@ -3,6 +3,7 @@ package com.example.game_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ClipDrawable;
 import android.os.Bundle;
@@ -16,6 +17,15 @@ public class GameActivity extends AppCompatActivity {
 
     static boolean isHalfShown = true;
 
+    static int radiationIntNow = 5000;
+
+    static int radiationInt = 5000;
+
+    ClipDrawable black_radiation;
+    ClipDrawable black_human;
+    ClipDrawable black_gun;
+    ClipDrawable black_money;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,10 +33,21 @@ public class GameActivity extends AppCompatActivity {
         ActivityGame2Binding binding = ActivityGame2Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        ClipDrawable black_radiation = (ClipDrawable) binding.radiation.getDrawable();
-        ClipDrawable black_human = (ClipDrawable) binding.human.getDrawable();
-        ClipDrawable black_gun = (ClipDrawable) binding.gun.getDrawable();
-        ClipDrawable black_money = (ClipDrawable) binding.money.getDrawable();
+        black_radiation = (ClipDrawable) binding.radiation.getDrawable();
+        black_human = (ClipDrawable) binding.human.getDrawable();
+        black_gun = (ClipDrawable) binding.gun.getDrawable();
+        black_money = (ClipDrawable) binding.money.getDrawable();
+        binding.gunBig.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.gunSmall.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.radBig.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.radSmall.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.humBig.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.humSmall.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.monBig.setCardBackgroundColor(Color.TRANSPARENT);
+        binding.monSmall.setCardBackgroundColor(Color.TRANSPARENT);
+
+        Transformation tr = new Transformation();
+        tr.start();
 
 
         binding.yesButton.setOnClickListener(new View.OnClickListener() {
@@ -34,21 +55,49 @@ public class GameActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (isHalfShown) {
                     black_gun.setLevel(10000);
-                    black_radiation.setLevel(10000);
+                    binding.gunSmall.setCardBackgroundColor(Color.WHITE);
+                    radiationInt = 10000;
+                    binding.radSmall.setCardBackgroundColor(Color.WHITE);
                     black_human.setLevel(10000);
+                    binding.humSmall.setCardBackgroundColor(Color.WHITE);
                     black_money.setLevel(10000);
+                    binding.monSmall.setCardBackgroundColor(Color.WHITE);
                     isHalfShown = false;
                 } else {
                     black_gun.setLevel(5000);
-                    black_radiation.setLevel(5000);
+                    binding.gunBig.setCardBackgroundColor(Color.WHITE);
+                    radiationInt = 5000;
+                    binding.radBig.setCardBackgroundColor(Color.WHITE);
                     black_human.setLevel(5000);
+                    binding.humBig.setCardBackgroundColor(Color.WHITE);
                     black_money.setLevel(5000);
+                    binding.monBig.setCardBackgroundColor(Color.WHITE);
                     isHalfShown = true;
                 }
             }
         });
-
-
-
     }
+
+
+    class Transformation extends Thread {
+        @Override
+        public void run() {
+            super.run();
+            while (true) {
+                if (radiationIntNow > radiationInt) {
+                    radiationIntNow -= 10;
+                } else if (radiationIntNow < radiationInt) {
+                    radiationIntNow += 10;
+                }
+                black_radiation.setLevel(radiationIntNow);
+                try {
+                    sleep(1);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    }
+
+
 }
